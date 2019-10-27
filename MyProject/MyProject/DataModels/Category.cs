5412be3c5c096ed8c5cf32f5961e519f593b7ae5
Hien Luong <hyhien@gmail.com>
2019-10-27 09:40:52 +0700

    Init Project

---
+++ MyProject/MyProject/DataModels/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.DataModels
+{
+    public class Category
+    {
+        public int CaterogyId { get; set; }
+        public string CaterogyName { get; set; }
+        public int? ParentCategoryId { get; set; }
+    }
+}
---
+++ MyProject/MyProject/ViewComponents/CategoryMenu.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.DataModels;
+using MyProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.ViewComponents
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepo _categoryRepo;
+        public CategoryMenu(ICategoryRepo categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+        }
+
+        /*Chỉ có 1 action duy nhất là Invoke()*/
+        public IViewComponentResult Invoke()
+        {         
+            return View("Default", _categoryRepo.GetAll());
+        }
+    }
+}
