4501c657b13173a166d5ad8dcb5a61418534d411
Hien <hyhien@gmail.com>
2019-11-02 17:16:10 +0700

    CRUD Category

---
+++ MyProject/MyProject/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace MyProject.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryBo _categoryBo;
+        public CategoriesController(ICategoryBo categoryBo)
+        {
+            _categoryBo = categoryBo;
+        }
+        public IActionResult Index()
+        {
+            return View(_categoryBo.GetAll());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CategoryModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _categoryBo.Insert(model);
+                    return RedirectToAction("Index");
+                }
+                catch(Exception ex)
+                {
+
+                    ModelState.AddModelError("loi", "Có lỗi khi thực hiện.");
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Edit(string id)
+        {
+            var category = _categoryBo.GetById(id);
+        }
+    }
+}
